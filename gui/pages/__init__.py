518965f43d5a84e92e519d8095bcee8e8b872f0c
RanjitK <ranjit.jhu@gmail.com>
2013-05-29 13:12:10 -0400

    added dual reg, logo and vertices timeseries

--- gui/pages/__init__.py
+++ gui/pages/__init__.py
@@ -9,9 +9,9 @@ from .centrality import CentralitySettings, Centrality
 from .alff import ALFF, ALFFSettings
 from .smoothing import Smoothing, SmoothingSettings
 from .filtering import Filtering, FilteringSettings
-from .timeseries import TimeSeries, ROITimeseries, VOXELTimeseries, SpatialRegression, GenerateSeeds
+from .timeseries import TimeSeries, ROITimeseries, VOXELTimeseries, SpatialRegression, GenerateSeeds, VerticesTimeSeries
 from .group_analysis import GroupAnalysis, GPASettings, BASCSettings, BASC, CWAS, CWASSettings
-
+from .dualreg import DualRegression, DualRegressionOptions
 
 
 __all__ = ['Anatomical', 'AnatomicalPreprocessing', \
@@ -26,6 +26,7 @@ __all__ = ['Anatomical', 'AnatomicalPreprocessing', \
            'Smoothing', 'SmoothingSettings',\
            'Filtering', 'FilteringSettings',\
            'TimeSeries', 'ROITimeseries', 'VOXELTimeseries', \
-           'SpatialRegression', 'GenerateSeeds',\
+           'SpatialRegression', 'GenerateSeeds', 'VerticesTimeSeries',\
            'GroupAnalysis', 'GPASettings', 'BASCSettings',\
-           'BASC', 'CWAS', 'CWASSettings']
+           'BASC', 'CWAS', 'CWASSettings'\
+           'DualRegression', 'DualRegressionOptions']
