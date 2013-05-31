c8bd1e49ae242753c5fc3790fd4a42611e2ff416
danlurie <danjlurie@gmail.com>
2013-05-30 20:03:56 -0400

    Mostly changes to Group Analysis and FSL Model Creator

--- gui/utils/modelconfig_window.py
+++ gui/utils/modelconfig_window.py
@@ -14,7 +14,7 @@ class ModelConfig(wx.Frame):
     def __init__(self, parent):
 
         wx.Frame.__init__(
-            self, parent=parent, title="FSL Model Setup", size=(900, 590))
+            self, parent=parent, title="CPAC - Create New FSL Model", size=(900, 590))
 
         mainSizer = wx.BoxSizer(wx.VERTICAL)
 
@@ -24,107 +24,100 @@ class ModelConfig(wx.Frame):
 
         self.page = generic_class.GenericClass(self.window, "FSL Model Setup")
 
-        self.page.add(label="SubjectList FilePath",
+        self.page.add(label="Subject List ",
                       control=control.COMBO_BOX,
                       name="subjectListFile",
                       type=dtype.STR,
-                      comment="Specify the full path to subject list for this model",
-                      values='/path/to/subject_list_group_analysis.txt')
+                      comment="Full path to a list of subjects to be included in the model.\n\nThis should be a text file with one subject per line.\n\nTip 1: A list in this format contaning all subjects run through CPAC was generated along with the main CPAC subject list (see subject_list_group_analysis.txt).\n\nTIp 2: An easy way to manually create this file is to copy the subjects column from your Regressor/EV spreadsheet.",
+                      values="")
 
-        self.page.add(label="Phenotypic Filepath",
+        self.page.add(label="EV File ",
                       control=control.COMBO_BOX,
                       name="phenotypicFile",
                       type=dtype.STR,
-                      comment="Specify the full path to template_phenotypic.csv.",
-                      values="/path/to/template_phenotypic.csv")
+                      comment="Full path to a .csv file containing EV information for each subject.\n\nTip: A file in this format (containing a single column listing all subjects run through CPAC) was generated along with the main CPAC subject list (see template_phenotypic.csv).",
+                      values="")
 
-        self.page.add(label="Subject Column",
+        self.page.add(label="Subject Column Name ",
                       control=control.TEXT_BOX,
                       name="subjectColumn",
                       type=dtype.STR,
-                      comment="Column Specifying subject id in phenotypic csv",
-                      values="subject_id",
+                      comment="Name of the subjects column in your EV file.",
+                      values="",
                       style=wx.EXPAND | wx.ALL,
                       size=(160, -1))
 
-        self.page.add(label="Columns/EV in Model",
+        self.page.add(label="EVs to Include ",
                       control=control.TEXT_BOX,
                       name="columnsInModel",
                       type=dtype.LSTR,
-                      comment="Specify which phenotypic variables and regressors to include in model \n"
-                            "These names should match column names in the template phenotypic csv",
-                      values="AgeAtScan, MeanFD, site, DxGroup",
+                      comment="Specify the names of columns in your EV file that you would like to include in this model.\n\nColumn names should be separated by commas and appear exactly as they do in your EV file.",
+                      values="",
                       style=wx.EXPAND | wx.ALL,
                       size=(600, -1))
 
-        self.page.add(label="Categorical Vs Directional",
+        self.page.add(label="EV Type ",
                       control=control.TEXT_BOX,
                       name="categoricalVsDirectional",
                       type=dtype.LNUM,
-                      comment="Set a type for each of the variables/regressors entered above.\n"
-                            "1 = categorical, 0 = directional",
-                      values="0,0,1,1",
+                      comment="Specify whether each of the EVs in this model should be treated as categorical or continuous.\n\nTo do this, place a 1 (categorical) or 0 (continuous) in the same list position as the corresponding EV.\n\nFor example, if the EVs to include were:\nage, sex, diagnosis, mean_fd\n\nOne might specify:\n0,1,1,0",
+                      values="",
                       style=wx.EXPAND | wx.ALL,
                       size=(160, -1))
 
-        self.page.add(label="DeMean",
+        self.page.add(label="Demean ",
                       control=control.TEXT_BOX,
                       name="deMean",
                       type=dtype.LNUM,
-                      comment="Specify whether to de-mean each column",
-                      values="1,1,0,0",
+                      comment="Specify whether to demean each of the EVs in this model.\n\nTo do this, place a 1 (demean) or 0 (don't demean) in the same list position as the corresponding EV.\n\nFor example, if the EVs to include were:\nage, sex, diagnosis, mean_fd\n\nOne might specify:\n1,0,0,1\n\nNote that only continuous EV's should be demeaned.",
+                      values="",
                       style=wx.EXPAND | wx.ALL,
                       size=(160, -1))
 
-        self.page.add(label="Contrast File for the Model",
+        self.page.add(label="Contrast File ",
                       control=control.COMBO_BOX,
                       name="contrastFile",
                       type=dtype.STR,
-                      comment="Full path to a contrast file. Columns names should be -\n"
-                      "'columnnames__value' for categorical variables & 'columnname' for directional variables.\n"
-                      "Also, f_test_1 f_test_2 ... f_test_n for n f tests. if no ftest then don't specify this column.\n"
-                      "Note:- if you want to drop a column from the model then don't mention the column name.",
-                      values='/path/to/contrasts.csv')
+                      comment="Full path to a .csv file containing contrasts to be applied to this model.\n\nWhen specifying EVs in this file:\n\n- Continuous EVs should appear the same as their corresponding column name in the EV file.\n\n- Categorical EVs must be split into multiple columns (one for each category), with names of the format EVname__N (e.g. diagnosis__1, diagnosis__2, diagnosis__3)\n\nIf you wish to include F-tests in your model, create a column for each desired F-test, with names in the format f_test_1, f_test_2, etc.",
+                      values="")
 
-        self.page.add(label="Grouping Variable",
-                      control=control.TEXT_BOX,
-                      name="groupingVariable",
-                      type=dtype.STR,
-                      comment="Column name/EV which is used to assign different groups for the model.For more information\n"
-                            "Please refer to http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FEAT/UserGuide#Appendix_A:_Brief_Overview_of_GLM_Analysis",
-                      values="None",
-                      size=(160, -1))
 
-        self.page.add(label="Model Group Variance Seperately:",
+        self.page.add(label="Model Group Variances Seperately ",
                       control=control.CHOICE_BOX,
                       name='modelGroupVariancesSeparately',
                       type=dtype.NUM,
-                      comment="This variable is used to setup different groups for different variances.\n"
-                      "When this option is set to 1, you need to specify the group column name below from the phenotypic file.\n"
-                      "This will result in giving different group number to different groups in the .grp file",
+                      comment="Specify whether FSL should model the variance for each group separately.\n\nIf this option is enabled, you must specify a grouping variable below.",
                       values=["Off", "On"])
 
-        self.page.add(label="Model Name",
+        self.page.add(label="Grouping Variable ",
+                      control=control.TEXT_BOX,
+                      name="groupingVariable",
+                      type=dtype.STR,
+                      comment="The name of the EV that should be used to group subjects when modeling variances.\n\nIf you do not wish to model group variances separately, set this value to None.",
+                      values="None",
+                      size=(160, -1))
+
+        self.page.add(label="Model Name ",
                       control=control.TEXT_BOX,
                       name="modelName",
                       type=dtype.STR,
-                      comment="Specify a name for the model",
-                      values="my_model",
+                      comment="Specify a name for the new model.",
+                      values="",
                       size=(200, -1))
 
-        self.page.add(label="Output Model Files Directory",
+        self.page.add(label="Output Directory ",
                       control=control.DIR_COMBO_BOX,
                       name="outputModelFilesDirectory",
                       type=dtype.STR,
-                      comment="Path to directory where the output model files will be put.",
+                      comment="Full path to the directory where CPAC should place model files.",
                       values=os.getcwd())
 
-        self.page.add(label="Output Model CSV File",
+        self.page.add(label="Model CSV File Name ",
                       control=control.TEXT_BOX,
                       name="outputModelFile",
                       type=dtype.STR,
-                      comment="Specify teh path for the model file .csv to be output. This file will be created by the tool.",
-                      values='./model_fsl.csv',
+                      comment="In addition to the standard FSL model files, CPAC will output a .csv containing the subjects and EVs specified above.\n\nColumn names in this file will be the same as in the contrasts file, and will have been demeaned as specified.",
+                      values="",
                       style=wx.EXPAND | wx.ALL,
                       size=(600, -1),
                       validation_req = False)
@@ -136,7 +129,7 @@ class ModelConfig(wx.Frame):
         btnPanel = wx.Panel(self.panel, -1)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-        run = wx.Button(btnPanel, ID_RUN, "CREATE MODEL", (
+        run = wx.Button(btnPanel, ID_RUN, "Create Model", (
             280, -1), wx.DefaultSize, 0)
         self.Bind(wx.EVT_BUTTON, lambda event: self.save(
             event, 'run'), id=ID_RUN)
@@ -145,17 +138,17 @@ class ModelConfig(wx.Frame):
         buffer = wx.StaticText(btnPanel, label="\t\t\t\t\t\t")
         hbox.Add(buffer)
 
-        cancel = wx.Button(btnPanel, wx.ID_CANCEL, "CANCEL", (
+        cancel = wx.Button(btnPanel, wx.ID_CANCEL, "Cancel", (
             220, 10), wx.DefaultSize, 0)
         self.Bind(wx.EVT_BUTTON, self.cancel, id=wx.ID_CANCEL)
         hbox.Add(cancel, 0, flag=wx.LEFT | wx.BOTTOM, border=5)
 
-        load = wx.Button(btnPanel, wx.ID_ADD, "LOAD Settings", (
+        load = wx.Button(btnPanel, wx.ID_ADD, "Load Settings", (
             200, -1), wx.DefaultSize, 0)
         self.Bind(wx.EVT_BUTTON, self.load, id=wx.ID_ADD)
         hbox.Add(load, 0.6, flag=wx.LEFT | wx.BOTTOM, border=5)
 
-        save = wx.Button(btnPanel, wx.ID_SAVE, "SAVE Settings", (
+        save = wx.Button(btnPanel, wx.ID_SAVE, "Save Settings", (
             200, -1), wx.DefaultSize, 0)
         self.Bind(wx.EVT_BUTTON, lambda event: self.save(
             event, 'save'), id=wx.ID_SAVE)
