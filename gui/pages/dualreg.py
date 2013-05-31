97f6e529eafddc80d110ca323a61204d74d4fc0d
Ranjitk <ranjit.jhu@gmail.com>
2013-05-31 00:58:56 -0400

    test

--- gui/pages/dualreg.py
+++ gui/pages/dualreg.py
@@ -44,5 +44,6 @@ class DualRegressionOptions(wx.ScrolledWindow):
         self.page.set_sizer()
         parent.get_page_list().append(self)
         
+
     def get_counter(self):
         return self.counter
